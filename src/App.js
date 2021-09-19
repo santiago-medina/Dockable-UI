8b3677b04fd7d74ec8a4365074a0828d71446118
santiago-medina <santiago.medina@gmail.com>
2021-09-19 09:49:10 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+import RHG from './resizable-holy-grail';
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <RHG></RHG>
+    </div>
+  );
+}
